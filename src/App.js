a46fd29f8e7cec60dfa4483e115c1f962c6c6889
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-12-27 17:38:59 -0600

    hero and cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "#fff",
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      fontSize: "3em"
+    }
+  },
+  blogsContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3)
+  },
+  card: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 240
+  },
+  cardActions: {
+    display: "flex",
+    margin: "0 10px",
+    justifyContent: "space-between"
+  },
+  author: {
+    display: "flex"
+  },
+  paginationContainer: {
+    display: "flex",
+    justifyContent: "center"
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="primary">
+            Programundo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.hero}>
+        <Box>Programundo</Box>
+      </Box>
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+        <Typography variant="h4" className={classes.blogTitle}>
+          Artículos
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/images/docker.png"
+                  title="Docker"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Docker Django
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Contenedor Docker + Nginx + Gunicorn
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="/images/me.jpg"/>
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Santiago Castañón Arvizu
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      27 Diciembre, 2021
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
