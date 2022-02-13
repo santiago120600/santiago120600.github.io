613c7e66bca31bd107f7653b72eaffc1aa559418
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2022-02-13 17:22:09 -0600

    added react-snap

--- src/index.js
+++ src/index.js
@@ -7,16 +7,31 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import theme from "./theme";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+    rootElement);
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+    rootElement
+  );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import {Helmet} from "react-helmet";
 
 const useStyles = makeStyles((theme) => ({
     title:{
@@ -17,6 +18,11 @@ function NotFound() {
     const classes = useStyles();
 
     return (
+        <>
+        <Helmet>
+            <title>404</title>
+            <meta name="description" content="No encontrado"/>
+        </Helmet>
         <Box
             className={classes.mainContainer}
             display="flex"
@@ -29,6 +35,7 @@ function NotFound() {
                 404
             </Typography>
         </Box>
+        </>
     );
 }
 export default NotFound;
